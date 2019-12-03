9c8ea0309cc01e3bbe508c2a9da7b036bd646f01
Your Name <you@example.com>
2019-12-03 10:34:16 +0100

    staet project

---
+++ Canzoni_con_testo_scorrevole.py
@@ -0,0 +1,12 @@
+something = ["Pamplona","Supreme","Stavo Pensando A Te","20 Anni(Peso)"] 
+
+
+class Canzoni:
+    def __init__  (author,producer,label,duration,title,text)
+    self.author= author
+    self.producer=producer
+    self.label=label
+    self.duration=duration
+    self.title=title
+    self.text=text
+   
